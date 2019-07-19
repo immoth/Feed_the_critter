ea4c3220b46c9639f6c76f976b40f73eae6ed230
John P. T. Stenger <33187634+immoth@users.noreply.github.com>
2019-07-19 14:23:12 -0400

    Add files via upload

---
+++ evo_0.1/evo_0.1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace evo_0._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Stop = new System.Windows.Forms.Button();
+            this.timerF = new System.Windows.Forms.Timer(this.components);
+            this.crith = new System.Windows.Forms.PictureBox();
+            this.food = new System.Windows.Forms.PictureBox();
+            this.crit = new System.Windows.Forms.PictureBox();
+            this.timerHunger = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.crith)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(22, 13);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(22, 43);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // timerF
+            // 
+            this.timerF.Interval = 10;
+            this.timerF.Tick += new System.EventHandler(this.timerF_Tick);
+            // 
+            // crith
+            // 
+            this.crith.Image = global::evo_0._1.Properties.Resources.crit_h;
+            this.crith.Location = new System.Drawing.Point(215, 164);
+            this.crith.Name = "crith";
+            this.crith.Size = new System.Drawing.Size(90, 73);
+            this.crith.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.crith.TabIndex = 4;
+            this.crith.TabStop = false;
+            this.crith.Visible = false;
+            // 
+            // food
+            // 
+            this.food.Image = global::evo_0._1.Properties.Resources.food;
+            this.food.Location = new System.Drawing.Point(766, 702);
+            this.food.Name = "food";
+            this.food.Size = new System.Drawing.Size(27, 23);
+            this.food.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.food.TabIndex = 3;
+            this.food.TabStop = false;
+            this.food.Visible = false;
+            // 
+            // crit
+            // 
+            this.crit.Image = global::evo_0._1.Properties.Resources.crit0;
+            this.crit.Location = new System.Drawing.Point(215, 164);
+            this.crit.Name = "crit";
+            this.crit.Size = new System.Drawing.Size(90, 73);
+            this.crit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.crit.TabIndex = 0;
+            this.crit.TabStop = false;
+            // 
+            // timerHunger
+            // 
+            this.timerHunger.Enabled = true;
+            this.timerHunger.Interval = 5000;
+            this.timerHunger.Tick += new System.EventHandler(this.timerHunger_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(479, 504);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(793, 726);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.crith);
+            this.Controls.Add(this.food);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.crit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.crith)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox crit;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.PictureBox food;
+        private System.Windows.Forms.Timer timerF;
+        private System.Windows.Forms.PictureBox crith;
+        private System.Windows.Forms.Timer timerHunger;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ evo_0.1/evo_0.1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace evo_0._1
+{
+    public partial class Form1 : Form
+    {
+        public void RandomDrawing()
+        {
+            int width = 50, height = 50;
+            Bitmap bmp = new Bitmap(width, height);
+            Random brand = new Random();
+            for (int by = 0; by < height; by++)
+            {
+                for (int bx = 0; bx < width; bx++)
+                {
+                    int a = brand.Next(256);
+                    int r = brand.Next(256);
+                    int g = brand.Next(256);
+                    int b = brand.Next(256);
+
+                    bmp.SetPixel(bx, by, Color.FromArgb(a, r, g, b));
+                }
+            }
+            pictureBox1.Image = bmp;
+            pictureBox1.Visible = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 550;
+            this.Height = 550;
+
+            RandomDrawing();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+
+        Random rnd = new Random();
+        int px = 0;
+        int py = 0;
+        int rbx = 0;
+        int rby = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //moves the imported image picture box
+            px += rnd.Next(-1, 2);
+            py += rnd.Next(-1, 2);
+            if (crit.Left < 10) { px = 5; }
+            if (crit.Left > 400) { px = -5; }
+            if (crit.Top < 10) { py = 5; }
+            if (crit.Top > 420) { py = -5; }
+            crit.Left += rnd.Next(-1 + px, 2 + px);
+            crit.Top += rnd.Next(-1 + py, 2 + py);
+
+            crith.Left = crit.Left;
+            crith.Top = crit.Top;
+
+            //moves the random colored picture box
+            //rbx += rnd.Next(-1, 2);
+            //rby += rnd.Next(-1, 2);
+            //if (pictureBox1.Left < 10) { rbx = 5; }
+            //if (pictureBox1.Left > 400) { rbx = -5; }
+            //if (pictureBox1.Top < 10) { rby = 5; }
+            //if (pictureBox1.Top > 420) { rby = -5; }
+            //pictureBox1.Left += rnd.Next(-1 + rbx, 2 + rbx);
+            //pictureBox1.Top += rnd.Next(-1 + rby, 2 + rby);
+            
+
+            if (Math.Abs(food.Top-crit.Top)<100)
+            {
+                if(Math.Abs(food.Left-crit.Left)<100)
+                {
+                    timerF.Enabled = false;
+                    food.Visible = false;
+                    food.Top = 600;
+                    crith.Visible = true;
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.Black, 3);
+            g.DrawRectangle(myPen, 2, 2, 500, 500);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            timerF.Enabled = true;
+            food.Top = e.Y;
+            food.Left = e.X;
+        }
+
+        private void timerF_Tick(object sender, EventArgs e)
+        {
+            if(food.Top < 470)
+            {
+                food.Visible = true;
+                food.Top += 2;
+            }
+            else
+            {
+                food.Visible = false;
+                food.Top = 600;
+            }
+
+            if (Math.Abs(food.Top - pictureBox1.Top) < 10)
+            {
+                if (Math.Abs(food.Left - pictureBox1.Left) < 100)
+                {
+                    timerF.Enabled = false;
+                    food.Visible = false;
+                    food.Top = 600;
+                    RandomDrawing();
+                }
+            }
+
+        }
+
+        private void timerHunger_Tick(object sender, EventArgs e)
+        {
+            crith.Visible = false;
+        }
+    }
+}
